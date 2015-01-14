9c8031916f36c0b0b8bd80892c09b950597c67fd
Alexander O'Han <xander@Alexanders-MacBook-Pro.local>
2015-01-14 22:54:19 +1100

    Working version of MTA homework. Stuck on trying to list stations on journey

--- xander/mta.rb
+++ xander/mta.rb
@@ -1,77 +1,62 @@
 require 'pry'
 
-# Students should create a program that models a simple subway system.
+lines = {	
+	"Line N" => ["8th", "Union Square", "23rd", "28th", "34th", "Times Square"],
+	"Line L" => ["1st", "3rd", "Union Square", "6th", "8th"],
+	"Line 6" => ["Astor Place", "Union Square", "23rd", "28th", "33rd", "Grand Central"]
+}
 
-# The program takes the line and stop that a user is getting on at and the line and stop that user is getting off at and prints the total number of stops for the trip.
-
-# There are 3 subway lines:
-# The N line has the following stops: Times Square, 34th, 28th, 23rd, Union Square, and 8th
-# The L line has the following stops: 8th, 6th, Union Square, 3rd, and 1st
-# The 6 line has the following stops: Grand Central, 33rd, 28th, 23rd, Union Square, and Astor Place.
-# All 3 subway lines intersect at Union Square, but there are no other intersection points.
-# For example, this means the 28th stop on the N line is different than the 28th street stop on the 6 line, so you'll have to differentiate this when you name your stops in the arrays.
-
-# Hints:
-
-# Consider diagraming the lines by sketching out the subway lines and their stops and intersection.
-
-# Make subway lines are keys in a hash, while the values are an array of all the stops on each line.
-
-# The key to the lab is to find the intersection of the lines at Union Square.
-
-# Make sure the stops that are the same for different lines have different names (i.e. 23rd on the N and on the 6 need to be differentiated)
+puts lines.keys
+print "Type which line are you on?: "
+current_line = gets.chomp
 
 
+puts lines[current_line]
+print "Which station are you at?: "
+current_station = gets.chomp
 
+puts lines.keys
+print "What is your destination line?: "
+destination_line = gets.chomp
 
-# hash of lines with arrays of the stations on each line
+puts lines[destination_line]
+print "What is your destination station?: "
+destination_station = gets.chomp
 
-lines = {	"LineN" => ["8", "Union_Square", "23", "28", "34", "Times_Square"], 
-			"LineL" => ["1", "3", "Union_Square", "6", "8"],
-			"Line6" => ["Astor_Place", "Union_Square", "23", "28", "33", "Grand_Central"]
-		}
+# calculating the number of stops between current and destination station
 
-# determine on which line the user is currently located
-def starting_line
-	print "Which line are you on?: "
-	current_line = gets.chomp
+stops_to_union_square = (lines[current_line].index(current_station) - lines[current_line].find_index("Union Square")).abs
+stops_from_union_square = (lines[destination_line].index(destination_station) - lines[destination_line].find_index("Union Square")).abs
+number_of_stops = (stops_to_union_square + stops_from_union_square)
+
+# This is the output for the user
+
+puts "Thank you for using Crappy Subway App"
+puts "Your travel directions are as follows: "
+puts "You are travelling from #{current_station} station on #{current_line} to #{destination_station} station on #{destination_line}."
+
+# this stipulates if the user needs to change lines
+
+if 
+	current_line == destination_line
+	puts "This route does not require changing trains. Please remain on #{current_line}."
+	
+	else
+		puts "This route requires you to change trains at Union Square to #{destination_line}."
 end
 
-# determine at which station the user is currently located
-def starting_station
-	print "Which station are you at?: "
-	current_station = gets.chomp
-end
-
-# determine the destination line of the user 
-def destination_line
-	print "What is your destination line?: "
-	destination_line = gets.chomp
-end
-
-# determine the destination station of the user
-def destination_station
-	print "What is your destination station?: "
-	destination_station = gets.chomp
-end
+puts "The total number of stations you will pass before you arrive at #{destination_station} station on #{destination_line} is #{ (number_of_stops - 1) }."
 
 
+print lines[current_line]
 
 
+binding.pry 
+
+puts "The stations you will pass are: "
 
 
-
-
-
-
-binding.pry
-
-
-# construct method asking user for input
-	# current_line {1. LineN, 2. LineL, 3. Line6}
-	# current_station {show array of stations within each line}
-	# destination_line {1. LineN, 2. LineL, 3. Line6}
-	# destination_station {show array of stations within each line}
+ 
 
 
 
