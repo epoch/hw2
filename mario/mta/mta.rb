03e06166c66ab5d9d5d7a72e91a7f1eb64b98aec
Mario Martinez <martimatix@Marios-MacBook-Pro.local>
2015-01-14 17:29:17 +1100

    Add other subway lines

--- mario/mta/mta.rb
+++ mario/mta/mta.rb
@@ -1,10 +1,15 @@
-n_line = ["Time Square", "34th", "28th", "23rd", "Union Square", "8th"]
+subways = {
+	:n_line => ["Time Square", "34th", "28th N", "23rd N", "Union Square", "8th N"],
+	:l_line => ["8th L", "6th", "Union Square", "3rd", "1st"],
+	:six_line => ["Grand Central", "33rd", "28th 6", "23rd N", "Union Square", "Astor Place"]
+}
+
 
 start_station = "Union Square"
 finish_station = "34th"
 
-start_index = n_line.index(start_station)
-finish_index = n_line.index(finish_station)
+start_index = subways[:n_line].index(start_station)
+finish_index = subways[:n_line].index(finish_station)
 
 # Direction that the train travels
 direction = 1
@@ -16,8 +21,8 @@ puts "Journey commencing."
 arrived, current_index, counter = false, start_index, 0
 
 until arrived do 
-	puts n_line[current_index]
-	arrived = n_line[current_index] == n_line[finish_index]
+	puts subways[:n_line][current_index]
+	arrived = subways[:n_line][current_index] == subways[:n_line][finish_index]
 	current_index += direction
 	counter += 1
 end
