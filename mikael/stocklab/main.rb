f84f1aed62673693f1b8138d4ea28162a19c2027
salmino <mikael.salmela@live.com>
2015-01-19 17:05:05 +1100

    Added if/else statement passing nil to the form.

--- mikael/stocklab/main.rb
+++ mikael/stocklab/main.rb
@@ -1,8 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
-require 'yahoofinance'
+require "yahoofinance"
 
 get "/" do
-  @quotes = YahooFinance::get_quotes(YahooFinance::StandardQuote, params[:stock].upcase)[params[:stock].upcase].lastTrade
-  erb :lookup
+  if params[:stock] == nil
+    @quotes = nil
+    erb :lookup
+  else params[:stock] == !nil
+    @quotes = YahooFinance::get_quotes(YahooFinance::StandardQuote, params[:stock].upcase)[params[:stock].upcase].lastTrade
+    erb :lookup
+  end
 end
