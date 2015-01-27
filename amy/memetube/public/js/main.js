44f7e3408f53300c46a7b3fd6aaa046bda15e03a
Amy Simmons <amy__simmons@hotmailcom>
2015-01-28 10:58:53 +1100

    js removed and form edit fixed

--- amy/memetube/public/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('.navigation').click(function() {
-    // this removes the underline class from all other ".navigation" links.
-    $('.navigation').removeClass('currentlyActive');
-
-    // this makes the one that was clicked underlined
-    $(this).addClass('currentlyActive');
-  });
-});
