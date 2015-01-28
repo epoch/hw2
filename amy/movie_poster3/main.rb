951bdb8eb7d4f548006712c36db2b34da40875b4
Amy Simmons <amy__simmons@hotmailcom>
2015-01-29 10:47:07 +1100

    Movie posters redone with local database

---
+++ amy/movie_poster3/main.rb
@@ -0,0 +1,106 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'HTTParty'
+require 'JSON'
+require 'pry'
+require 'active_record'
+
+
+ActiveRecord::Base.logger = Logger.new(STDERR) 
+# show sql in the terminal
+
+ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3', 
+    :database => "movies.db"
+)
+
+require_relative 'movie'
+
+# the above two lines establish the connection and load our movie class
+# active record lets you map a class to the table without specifying the name of the table
+
+
+get '/' do
+    erb :search
+end
+
+get '/results' do
+
+    title = params[:title].gsub(/\s/,'+')
+
+    imdbID = params[:imdbID]
+
+    type = params[:type]
+
+    url = "http://www.omdbapi.com/?s=#{title}"
+
+    raw_data = HTTParty.get url
+
+    movie_options = JSON.parse raw_data
+
+    @movie_options_array = movie_options['Search']
+
+
+    if movie_options["Error"]
+        erb :movie_error
+    else
+
+        if @movie_options_array.length == 1
+            redirect to("/poster?id=#{@movie_options_array[0]['imdbID']}")
+        else
+            erb :results
+        end
+    end
+end
+
+get '/poster' do
+
+@movie = Movie.find_by(:imdbID => params[:id])
+
+    if @movie
+
+        if @movie.poster == "N/A"
+            erb :poster_error
+        else
+            @image_url = @movie.poster
+            erb :poster
+        end
+
+    else
+
+        id = params[:id]
+
+        url = "http://www.omdbapi.com/?i=#{id}" 
+
+        raw_data = HTTParty.get url
+
+        movie_data = JSON.parse raw_data
+
+        @image_url = movie_data['Poster']
+
+
+        if movie_data['Poster'] == "N/A"
+            erb :poster_error
+        else
+
+            movie = Movie.new
+            movie.title = movie_data['Title']
+            movie.imdbID = movie_data['imdbID']
+            movie.poster = movie_data['Poster']
+            movie.response = movie_data['Response']
+            movie.save
+
+            erb :poster
+
+        end
+        
+    end
+
+end
+
+
+get '/pry' do
+    binding.pry
+end
+
+
---
+++ amy/movie_poster3/readme.txt
@@ -0,0 +1,34 @@
+Have a database for movies on your own local area
+
+So each movie you request only once from imdb
+
+You take the data, store it in the database yourselves
+
+And if somebody else comes looking for it later, you’ve kept a copy of all of that stuff
+
+One time it will go off to imbd and get the data
+
+The second time it will remember the poster
+
+Got to imdb once, to get list of all sharknado movies
+
+But keep a copy of whatever else in the database so we only need to request things  once
+
+Need two things for this:
+
+Create table movies
+Put your fields in it 
+In this table work out what things u care about from imbd to store
+
+And you will need a class called Movie
+
+So you can do things like movie.find
+
+
+
+
+at the point they click on the movie thy want, store it in the database
+
+.save on the database
+
+
