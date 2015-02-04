97917a8f4e1074d93c744610ccaa9dab70e74d88
SoxFace <sonyacorcoran@gmail.com>
2015-02-04 20:35:54 +1100

    W4D3 Bookshelf app with book and authors scaffold

--- sonya/W4/rails/bookshelf/app/controllers/authors_controller.rb
+++ sonya/W4/rails/bookshelf/app/controllers/authors_controller.rb
@@ -4,12 +4,16 @@ class AuthorsController < ApplicationController
   end
 
   def create
+    author = Author.create author_params
+    redirect_to author
   end
 
   def new
+    @author = Author.new
   end
 
   def edit
+    @author = Author.find params[:id]
   end
 
   def show
@@ -17,8 +21,20 @@ class AuthorsController < ApplicationController
   end
 
   def update
+    author = Author.find params[:id]
+    author.update author_params
+    redirect_to author
   end
 
   def destroy
+    author = Author.find params[:id]
+    author.destroy
+    redirect_to author_path
   end
+
+  private
+  def author_params
+    params.require(:author).permit(:name, :nationality, :dob, :image)
+  end
+
 end
--- sonya/W4/rails/bookshelf/config/routes.rb
+++ sonya/W4/rails/bookshelf/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root :to => 'authors#index'
-  resources :authors
+  resources :authors, :books
 end
