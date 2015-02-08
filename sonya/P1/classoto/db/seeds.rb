20a2f70acf289bc9e24b4885bef6af76da7feac2
SoxFace <sonyacorcoran@gmail.com>
2015-02-08 14:58:31 +1100

    Image displaying in index

--- sonya/P1/classoto/db/seeds.rb
+++ sonya/P1/classoto/db/seeds.rb
@@ -4,7 +4,7 @@ Cohort.destroy_all
 u1 = User.create(:name => 'Sonya', :password => 'chicken', :password_confirmation => 'chicken')
 u2 = User.create(:name => 'Laura', :password => 'chicken', :password_confirmation => 'chicken')
 u3 = User.create(:name => 'Lynette', :password => 'chicken', :password_confirmation => 'chicken')
-admin = User.create(:name => 'admin', :password => 'admin', :password_confirmation => 'admin', :is_admin => true)
+admin = User.create(:name => 'admin', :password => 'admin', :password_confirmation => 'admin', :image =>"http://upload.wikimedia.org/wikipedia/lv/1/14/Ralph_Wiggum.png", :is_admin => true)
 
 c1 = Cohort.create(:year => '2015')
 
