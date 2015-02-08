0aca6bd026fb66b77f04a4633c952c67181d8820
SoxFace <sonyacorcoran@gmail.com>
2015-02-08 15:28:11 +1100

    Added pages index with placeholders

--- sonya/P1/classoto/config/routes.rb
+++ sonya/P1/classoto/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
   resources :users, :only => [:new, :create, :index]
+  resources :pages
 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
