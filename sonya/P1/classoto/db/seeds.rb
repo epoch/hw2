3602617b8eeba8b6ead3516ce32d5fcac976d4fe
SoxFace <sonyacorcoran@gmail.com>
2015-02-08 15:11:21 +1100

    Added image to seed data to display on index page

--- sonya/P1/classoto/db/seeds.rb
+++ sonya/P1/classoto/db/seeds.rb
@@ -1,10 +1,10 @@
 User.destroy_all
 Cohort.destroy_all
 
-u1 = User.create(:name => 'Sonya', :password => 'chicken', :password_confirmation => 'chicken')
-u2 = User.create(:name => 'Laura', :password => 'chicken', :password_confirmation => 'chicken')
-u3 = User.create(:name => 'Lynette', :password => 'chicken', :password_confirmation => 'chicken')
-admin = User.create(:name => 'admin', :password => 'admin', :password_confirmation => 'admin', :image =>"http://upload.wikimedia.org/wikipedia/lv/1/14/Ralph_Wiggum.png", :is_admin => true)
+u1 = User.create(:name => 'Sonya', :password => 'chicken', :password_confirmation => 'chicken', :image => "student_placeholder.png")
+u2 = User.create(:name => 'Laura', :password => 'chicken', :password_confirmation => 'chicken', :image => "student_placeholder.png")
+u3 = User.create(:name => 'Lynette', :password => 'chicken', :password_confirmation => 'chicken', :image => "student_placeholder.png")
+admin = User.create(:name => 'admin', :password => 'admin', :password_confirmation => 'admin', :image => "student_placeholder.png", :is_admin => true)
 
 c1 = Cohort.create(:year => '2015')
 
