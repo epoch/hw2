2a8089d789163cf301422772d7d4b5ce42a92090
SoxFace <sonyacorcoran@gmail.com>
2015-02-09 09:46:34 +1100

    Created Update of CRUD

--- sonya/P1/classoto/app/controllers/users_controller.rb
+++ sonya/P1/classoto/app/controllers/users_controller.rb
@@ -18,6 +18,14 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find params[:id]
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
--- sonya/P1/classoto/config/routes.rb
+++ sonya/P1/classoto/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
-  resources :users, :only => [:new, :create, :index]
+  resources :users, :only => [:new, :create, :index, :show, :edit]
+  resources :pages
 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
