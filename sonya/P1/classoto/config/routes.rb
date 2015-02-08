adc953908fce8201bfe697305db1a277f8a6eee3
SoxFace <sonyacorcoran@gmail.com>
2015-02-09 10:31:40 +1100

    CRUD merged onto master

--- sonya/P1/classoto/config/routes.rb
+++ sonya/P1/classoto/config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
-<<<<<<< HEAD
   resources :users
-=======
-  resources :users, :only => [:new, :create, :index]
->>>>>>> photolist
   resources :pages
 
   get '/login' => 'session#new'
