20d84484f69d200158e6722a5a8fb8ff35e74eb8
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-16 21:05:30 +1100

    arrays

---
+++ mario/js/arrays/arrays.js
@@ -0,0 +1,8 @@
+var topChoices = function() {
+  var myTopChoices = ["Martini", "Negroni", "Gin and Tonic", "Tom Collins"]
+  for (var i = 0; i < myTopChoices.length; i++) {
+    console.log("My #" + (i + 1) + " choice is " + myTopChoices[i]);
+  };
+}
+
+topChoices();
