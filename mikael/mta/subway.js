05f0a017972ed375bd9aa1f6e87ad37b1ee66906
salmino <mikael.salmela@live.com>
2015-02-17 17:30:24 +1100

    mta straight copy from ruby edition

---
+++ mikael/mta/subway.js
@@ -0,0 +1,7 @@
+var subway = {
+  'N' : ['TIMES SQUARE', '34N', '28N', '23N', 'UNION STATION', '8N'],
+  'L' : ['8L', '6L', 'UNION STATION', '3L', '1L'],
+  'S' : ['GRAND CENTRAL', '33S', '28S', '23S', 'UNION STATION', 'ASTOR PLACE']
+};
+
+console.log(subway['N']);
