0cc4fd0d9a5b144689589be59c5b2a3e8e297dbe
xandero <alexander.ohan@gmail.com>
2015-02-17 17:13:56 +1100

    need to add user inputs

--- xander/MTAjs/mta.js
+++ xander/MTAjs/mta.js
@@ -12,24 +12,53 @@ var currentStation = userInputCurrent.slice(2);
 var destinationLine = userInputDestination.slice(0,1);
 var destinationStation = userInputDestination.slice(2);
 
+var currentStartIndex;
+var currentUnionSquare;
+var destinationUnionSquare;
+var destinationFinishIndex;
+var stationsPassed;
+var currentStationsPassed;
+var destinationStationsPassed;
+var changeLines;
+
 if (currentLine === destinationLine) {
-  var start_index = lines[currentLine].indexOf(currentStation);
-  var finish_index = lines[destinationLine].indexOf(destinationStation);
-  var stationsPassed;
-  if (start_index < finish_index) {
-    stationsPassed = lines[currentLine].slice(start_index, finish_index);
+  var startIndex = lines[currentLine].indexOf(currentStation);
+  var finishIndex = lines[destinationLine].indexOf(destinationStation);
+  changeLines = 'You will not be required to change lines on this journey.';
+  if (startIndex < finishIndex) {
+    stationsPassed = lines[currentLine].slice(startIndex, finishIndex);
   }
   else {
-    stationsPassed = lines[currentLine].slice(finish_index, start_index);
+    stationsPassed = lines[currentLine].slice(finishIndex, startIndex);
   }
-  console.log(stationsPassed);
+} else {
+  changeLines = 'You will need to change to line ' + destinationLine + ' at Union Square.';
+
+  currentStartIndex = lines[currentLine].indexOf(currentStation);
+  currentUnionSquare = lines[currentLine].indexOf("Union Square");
+  destinationUnionSquare = lines[destinationLine].indexOf("Union Square");
+  destinationFinishIndex = lines[destinationLine].indexOf(destinationStation); 
+
+  if (currentStartIndex < currentUnionSquare) {
+    currentStationsPassed = lines[currentLine].slice(currentStartIndex, currentUnionSquare);
+
+  } else  {
+    currentStationsPassed = lines[currentLine].slice(currentUnionSquare, currentStartIndex);
+    currentStationsPassed = currentStationsPassed.reverse();
+  }
+
+  if (destinationFinishIndex < destinationUnionSquare) {
+    destinationStationsPassed = lines[destinationLine].slice(destinationFinishIndex, (destinationUnionSquare - 1));
+    destinationStationsPassed = destinationStationsPassed.reverse();
+  
+  } else {
+    destinationStationsPassed = lines[destinationLine].slice((destinationUnionSquare + 1), destinationFinishIndex);
+  }
+  stationsPassed = currentStationsPassed + destinationStationsPassed;
 }
-var changeLines = 'You will not be required to change lines on this journey.';
 
-var current_start_index = lines[currentLine].indexOf(currentStation);
-var current_union_square = lines[currentLine].indexOf("Union Square");
-var destination_union_square = lines[destinationLine].indexOf("Union Square");
-var destination_finish_index = lines[destinationLine].indexOf(destinationStation); 
+var numberOfStops = (stationsPassed.length) - 1;
 
+console.log('You are travelling from' + currentStation + ' station on Line ' + currentLine + ', to ' + destinationStation + ' station on Line ' + destinationLine + '.' + changeLines + 'You will pass ' + numberOfStops + ' stations before you arrive at ' + destinationStation + ' station on Line ' + destinationLine + '.');
 
-
+console.log('Your journey will encompass the following stops: ' + stationsPassed + '.')
