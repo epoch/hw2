ecbf79c3c803d0e3962444d238042f420074ae48
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-17 16:19:09 +1100

    mta.js working version

--- mario/mta.js
+++ mario/mta.js
@@ -9,7 +9,7 @@ const LINES = {
 singleLine = {
   line : "n_line",
   startStation : "28th",
-  finishStation : "28th",
+  finishStation : "8th",
 
   startIndex : function() {
     return LINES[this.line].indexOf(this.startStation)
@@ -32,10 +32,48 @@ singleLine = {
       return (LINES[this.line].slice(this.finishIndex(), this.startIndex() + 1)).reverse()
     }
   }
-
 }
 
-console.log("Start index is " + singleLine.startIndex());
-console.log("Finish index is " + singleLine.finishIndex());
-console.log("The number of stops is " + singleLine.counter());
-console.log("The stations are " + singleLine.stations())
+mta = {
+  startLine : "n_line",
+  finishLine : "six_line",
+  startStation : "28th",
+  finishStation : "Grand Central",
+
+  journey : function () {
+    if (this.startLine === this.finishLine) {
+      return this.tripWithNoTransfers()
+    } else {
+      return this.tripWithTransfers()
+    }
+  },
+
+  tripWithNoTransfers : function() {
+    singleLine.line = this.startLine;
+    singleLine.startStation = this.startStation;
+    singleLine.finishStation = this.finishStation;
+    console.log("The number of stops is " + singleLine.counter());
+    console.log("The stations are " + singleLine.stations());
+  },
+
+  tripWithTransfers : function() {
+    singleLine.line = this.startLine;
+    singleLine.startStation = this.startStation;
+    singleLine.finishStation = "Union Square";
+    counter = singleLine.counter();
+    stations1 = singleLine.stations();
+
+    singleLine.line = this.finishLine;
+    singleLine.startStation = "Union Square";
+    singleLine.finishStation = this.finishStation;
+    counter += singleLine.counter();
+    stations2 = singleLine.stations();
+
+    console.log("The number of stops is " + counter);
+    console.log("The stop on " + this.startLine + " are " + stations1);
+    console.log("Transfer at Union Square.");
+    console.log("Then, the stop on " + this.finishLine + " are " + stations2);
+  }
+}
+
+mta.journey()
