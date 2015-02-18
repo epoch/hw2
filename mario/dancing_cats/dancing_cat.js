c6bde74cb0985ce9402600c9364c14123c37dcb4
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-19 09:18:34 +1100

    cat speeds up

--- mario/dancing_cats/dancing_cat.js
+++ mario/dancing_cats/dancing_cat.js
@@ -1,7 +1,9 @@
 var img = document.getElementsByTagName('img')[0];
 var startButton = document.getElementById('start-button');
+var speedButton = document.getElementById('speed-button');
 var stopButton = document.getElementById('stop-button');
 var timer = null;
+var speedFactor = 1;
 
 img.style.position = 'absolute';
 img.style.top = '100px';
@@ -19,12 +21,22 @@ var step = function() {
 };
 
 var watchKittyWalk = function() {
-  timer = window.setInterval(step, 50);
+  if (!timer) {
+  timer = window.setInterval(step, 50/speedFactor);
+  }
 };
 
 var stopKittyWalk = function () {
   window.clearInterval(timer);
+  timer = null;
 };
 
+var speedUpKitty = function () {
+  speedFactor++;
+  stopKittyWalk();
+  watchKittyWalk();
+}
+
 startButton.addEventListener('click', watchKittyWalk, false);
+speedButton.addEventListener('click', speedUpKitty, false);
 stopButton.addEventListener('click', stopKittyWalk, false);
