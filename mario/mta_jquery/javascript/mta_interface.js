773bcdcc8d64445a7266e76526bdf6cb0c7e21a4
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-22 10:07:20 +1100

    container cleared when data is entered

--- mario/mta_jquery/javascript/mta_interface.js
+++ mario/mta_jquery/javascript/mta_interface.js
@@ -1,14 +1,22 @@
 // jQuery newbie so comment overkill
 
 $(document).ready(function () {
+
+  // Object to store client choices
+  var mtaClient = {};
+
+
   // Find the container div in the html
   $container = $(".container");
 
-  // Iterate over Lines
+  // Create a div to hold menu items
+  var $menu = $('<div/>').addClass("menu");
+
+  // Iterate over Train Lines
   $.each( LINES, function( key, value ) {
 
     // Create a <ul></ul> with the class being the key
-    var $line = $('<ul/>').addClass(key);
+    var $subwayLine = $('<ul/>').addClass(key);
 
     // Iterate of the line (eg: N-line)
     $.each(value, function( index, value ) {
@@ -28,18 +36,25 @@ $(document).ready(function () {
       $li.append($radioBtn, $label);
 
       // Add the <li> to the <ul>
-      $line.append($li);
+      $subwayLine.append($li);
     }); 
-    $container.append($line);
+    $menu.append($subwayLine);
   });
 
   var $buttonNext = $("<button>").attr('id', 'nextButton').text("Next");
-  $container.append($buttonNext);
-
+  $menu.append($buttonNext);
+  $menu.addClass('animated fadeInRight');
+  $container.append($menu);
 
   $("#nextButton").click(function(){
-    var value = $("input[name=startStation]:checked").val()      
-    console.log(value)
+    var value = $("input[name=startStation]:checked").val().split('|')
+    mtaClient.startStation = value[0];
+    mtaClient.startLine = value[1];
+    $menu.addClass('animated fadeOutLeft');
+    console.log(mtaClient.startStation);
+    console.log(mtaClient.startLine);
+    // Remove $menu
+    $container.empty();
   });
 
 });
