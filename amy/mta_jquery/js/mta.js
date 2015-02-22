ce60bf0a84d21faee2136ba97b201c5f0d5f2f35
Amy Simmons <amy__simmons@hotmailcom>
2015-02-22 16:49:36 +1100

    stations and num stops displaying

--- amy/mta_jquery/js/mta.js
+++ amy/mta_jquery/js/mta.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         "6": ["Grand Central(6)", "33rd(6)", "28th(6)", "23rd(6)", "Union Square", "Astor Place(6)"]
     }
 
-    var submitDeparture = function(subwayLines){
+    var generateDepartureForm = function(subwayLines){
         $.each(subwayLines, function(key, value){
             
             var lineOn = key
@@ -20,9 +20,9 @@ $(document).ready(function(){
             };
         });
     }
-    submitDeparture(subwayLines);
+    generateDepartureForm(subwayLines);
 
-    var submitDestination = function(subwayLines){
+    var generateDestinationForm = function(subwayLines){
         $.each(subwayLines, function(key, value){
             
             var lineOff = key
@@ -36,48 +36,118 @@ $(document).ready(function(){
             };
         });
     }
-    submitDestination(subwayLines);
+    generateDestinationForm(subwayLines);
 
-    // var lineOn = function(){
-    //     var selectedLineOn = $('select.departure option:selected').parent().attr('label');
-    //     console.log (selectedLineOn);
-    // }
-    // $('.submit-departure').on('click', lineOn);
-
-    var getDepartureInfo = function(){
+    var listStations = function(){
+        
         var selectedLineOn = $('select.departure option:selected').parent().attr('label');
         var selectedStopOn = $('select.departure option:selected').val();
         var indexStopOn = subwayLines[selectedLineOn].indexOf(selectedStopOn);
         var indexUnionSquareOn = subwayLines[selectedLineOn].indexOf("Union Square");
-        console.log (selectedLineOn, selectedStopOn, indexStopOn, indexUnionSquareOn);
-    }
-    $('.submit-departure').on('click', getDepartureInfo);
+        // console.log (selectedLineOn, selectedStopOn, indexStopOn, indexUnionSquareOn);
 
-    // var lineOff = function(){
-    //     var selectedLineOff = $('select.destination option:selected').parent().attr('label');
-    //     console.log (selectedLineOff);
-    // }
-    // $('.submit-destination').on('click', lineOff);
-
-    var getDestinationInfo = function(){
         var selectedLineOff = $('select.destination option:selected').parent().attr('label');
         var selectedStopOff = $('select.destination option:selected').val();
         var indexStopOff = subwayLines[selectedLineOff].indexOf(selectedStopOff);
         var indexUnionSquareOff = subwayLines[selectedLineOff].indexOf("Union Square");
-        console.log (selectedLineOff, selectedStopOff, indexStopOff, indexUnionSquareOff);
-    }
-    $('.submit-destination').on('click', getDestinationInfo);
+        // console.log (selectedLineOff, selectedStopOff, indexStopOff, indexUnionSquareOff);
 
-    // var indexStopOn = subwayLines[lineOn].indexOf(stopOn);
-    // console.log
+        var numStops = $('<p></p>').addClass('num-stops');
+        numStops.appendTo('.container');
+        var stationsList = $('<ul></ul>').addClass('stations-list');
+        stationsList.appendTo('.container');
+        if(selectedLineOn === selectedLineOff){
 
-    // var indexStopOff = subwayLines[lineOff].indexOf(stopOff);
+            // list stations and stops for single line journey
 
-    // var indexUnionSquareOn = subwayLines[lineOn].indexOf("Union Square");
+            if(indexStopOn < indexStopOff){
+                
+                var totalStations = subwayLines[selectedLineOn].slice(indexStopOn, indexStopOff + 1);
+                console.log("There are " + (totalStations.length - 1) + " stops until you reach your destination:");
+                numStops.html("There are " + (totalStations.length - 1) + " stops until you reach your destination:");
 
-    // var indexUnionSquareOff = subwayLines[lineOff].indexOf("Union Square");
+                for ( var i = 0; i < totalStations.length; i++ ){
+                    console.log(totalStations[i]);
+                    $('<li>' +totalStations[i]+ '</li>').addClass('stations-name').appendTo(stationsList);
+                };
+
+            }else {
+                var totalStations = subwayLines[selectedLineOn].slice(indexStopOff, indexStopOn + 1);
+                var totalStations = totalStations.reverse();
+
+                console.log("There are " + (totalStations.length - 1) + " stops until you reach your destination:");
+                numStops.html("There are " + (totalStations.length - 1) + " stops until you reach your destination:");
+
+                for ( var i = 0; i < totalStations.length; i++ ){
+                    console.log(totalStations[i]);
+                    $('<li>' +totalStations[i]+ '</li>').addClass('stations-name').appendTo(stationsList);
+                };
+            }
+
+        }else {
+
+            // list stations and stops for changeover journey
+
+            if(indexStopOn < indexUnionSquareOn){
+                var stationsBeforeChangeoverArr = subwayLines[selectedLineOn].slice(indexStopOn, indexUnionSquareOn + 1);
+            }else if (indexStopOn > indexUnionSquareOn) {
+                var stationsBeforeChangeoverArr = subwayLines[selectedLineOn].slice(indexUnionSquareOn, indexStopOn + 1);
+                var stationsBeforeChangeoverArr = stationsBeforeChangeoverArr.reverse();
+            }
+
+            if(indexStopOff < indexUnionSquareOff){
+                var stationsAfterChangeoverArr = subwayLines[selectedLineOff].slice(indexStopOff, indexUnionSquareOff);
+                var stationsAfterChangeoverArr = stationsAfterChangeoverArr.reverse();
+            }else if (indexStopOff > indexUnionSquareOff){
+                var stationsAfterChangeoverArr = subwayLines[selectedLineOff].slice(indexUnionSquareOff + 1, indexStopOff + 1);
+            }
+
+            var totalStations = stationsBeforeChangeoverArr.concat(stationsAfterChangeoverArr);
+
+            console.log("There are " + (totalStations.length - 1) + " stops until you reach your destination:");
+            numStops.html("There are " + (totalStations.length - 1) + " stops until you reach your destination:");
+
+            for ( var i = 0; i < totalStations.length; i++ ){
+                console.log(totalStations[i]);
+                $('<li>' +totalStations[i]+ '</li>').addClass('stations-name').appendTo(stationsList);
+            };
+
+        }
+
+    };
+
+$('.submit').on('click', listStations);
 
 }); // end document.ready
 
 
-   
+    // var getDepartureInfo = function(){
+    //     selectedLineOn = $('select.departure option:selected').parent().attr('label');
+    //     selectedStopOn = $('select.departure option:selected').val();
+    //     indexStopOn = subwayLines[selectedLineOn].indexOf(selectedStopOn);
+    //     indexUnionSquareOn = subwayLines[selectedLineOn].indexOf("Union Square");
+    //     console.log (selectedLineOn, selectedStopOn, indexStopOn, indexUnionSquareOn);
+    // }
+
+    // var getDestinationInfo = function(){
+    //     selectedLineOff = $('select.destination option:selected').parent().attr('label');
+    //     selectedStopOff = $('select.destination option:selected').val();
+    //     indexStopOff = subwayLines[selectedLineOff].indexOf(selectedStopOff);
+    //     indexUnionSquareOff = subwayLines[selectedLineOff].indexOf("Union Square");
+    //     console.log (selectedLineOff, selectedStopOff, indexStopOff, indexUnionSquareOff);
+    // }
+
+    // var selectedLineOn 
+    // var selectedStopOn 
+    // var indexStopOn 
+    // var indexUnionSquareOn 
+    // var selectedLineOff 
+    // var selectedStopOff
+    // var indexStopOff 
+    // var indexUnionSquareOff 
+
+   // listStations(selectedLineOn, selectedStopOn, indexStopOn, indexUnionSquareOn, selectedLineOff, selectedStopOff, indexStopOff, indexUnionSquareOff);
+    // listStations(selectedLineOn, selectedStopOn, indexStopOn, indexUnionSquareOn, selectedLineOff, selectedStopOff, indexStopOff, indexUnionSquareOff);
+
+
+// $('.submit').on('click', getDepartureInfo, listStations);
