3a11a9533bd886e1ed2374bda36ded176a78db14
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-22 11:18:01 +1100

    finish station loads after start station menu

--- mario/mta_jquery/javascript/mta_interface.js
+++ mario/mta_jquery/javascript/mta_interface.js
@@ -5,57 +5,47 @@ $(document).ready(function () {
   // Object to store client choices
   var mtaClient = {};
 
-  // Find the container div in the html
   $container = $(".container");
 
-  // Create a div to hold menu items
-  var $menu = $('<div/>').addClass("menu");
+  var getStationInfo = function(startOrFinish) {
+    var $menu = $('<div/>').addClass("menu");
 
-  // Iterate over Train Lines
-  $.each( LINES, function( key, value ) {
+    $.each( LINES, function( key, value ) {
+      var $subwayLine = $('<ul/>').addClass(key);
+      $.each(value, function( index, value ) {
 
-    // Create a <ul></ul> with the class being the key
-    var $subwayLine = $('<ul/>').addClass(key);
+        var $li = $('<li/>');
+        var stationID = value + '|' + key
+        var $label = $("<label>").attr({for: stationID}).text(value);
+        var $radioBtn = $('<input type="radio">').attr({id: stationID, value: stationID, name: 'stationLine'});
 
-    // Iterate of the line (eg: N-line)
-    $.each(value, function( index, value ) {
+        $li.append($radioBtn, $label);
+        $subwayLine.append($li);
+      }); 
+      $menu.append($subwayLine);
+    });
 
-      // Create a new <li> element
-      var $li = $('<li/>');
+    var $buttonNext = $("<button>").attr('id', 'nextButton').text("Next");
+    $menu.append($buttonNext);
+    $menu.addClass('animated fadeInRight');
+    $container.append($menu);
 
-      var stationID = value + '|' + key
-
-      // Create a label
-      var $label = $("<label>").attr({for: stationID}).text(value);
-
-      // Create a radio button
-      var $radioBtn = $('<input type="radio">').attr({id: stationID, value: stationID, name: 'stationLine'});
-
-      // The contents of <li> is a station (value)
-      $li.append($radioBtn, $label);
-
-      // Add the <li> to the <ul>
-      $subwayLine.append($li);
-    }); 
-    $menu.append($subwayLine);
-  });
-
-  var $buttonNext = $("<button>").attr('id', 'nextButton').text("Next");
-  $menu.append($buttonNext);
-  $menu.addClass('animated fadeInRight');
-  $container.append($menu);
-
-  $("#nextButton").click(function(){
-    var value = $("input[name=stationLine]:checked").val().split('|')
-    mtaClient['startStation'] = value[0];
-    mtaClient['startLine'] = value[1];
-    $menu.addClass('animated fadeOutLeft');
-    console.log(mtaClient['startStation']);
-    console.log(mtaClient['startLine']);
-    // Remove $menu with a delay so we get fadeOut animation effect
-    setTimeout(function(){
-      $container.empty();
-    }, 2000);
-  });
-
+    $("#nextButton").click(function(){
+      var value = $("input[name=stationLine]:checked").val().split('|')
+      mtaClient[startOrFinish + 'Station'] = value[0];
+      mtaClient[startOrFinish + 'Line'] = value[1];
+      $menu.addClass('animated fadeOutLeft');
+      console.log(mtaClient[startOrFinish + 'Station']);
+      console.log(mtaClient[startOrFinish + 'Line']);
+      // Remove $menu with a delay so we get fadeOut animation effect
+      setTimeout(function(){
+        $container.empty();
+        if (startOrFinish === 'start') {
+          getStationInfo('finish');
+        }
+      }, 300);
+    });
+  }
+  
+  getStationInfo('start');
 });
