e831337d1c85e34af69ffadcc9e0669d93bf47fc
soxface <sonyacorcoran@gmail.com>
2015-02-23 20:53:50 +1100

    bootstrap added

--- sonya/W7/flickr_app/app/assets/javascripts/flickr.js
+++ sonya/W7/flickr_app/app/assets/javascripts/flickr.js
@@ -1,4 +1,22 @@
-var searchFlickr = function() {
+$(document).ready(function(){
+  // SEARCH
+  $('#search').on('click', searchFlickr);
+  $('#query').on('keypress', function(event){
+    if(event.which !== 13){
+      return;
+    } 
+    searchFlickr();
+  });
+
+  // CLEAR
+  $('#clear').on('click', clearPhotos);
+
+  // CONTROL SCROLL
+  $(window).on('scroll', _.throttle(lateLoad, 250));
+});
+
+// FLICKR API 
+var searchFlickr = function(event) {
   var query = $('#query').val();
 
   var flickrUrl = 'https://api.flickr.com/services/rest/?jsoncallback=?';
@@ -8,13 +26,49 @@ var searchFlickr = function() {
     api_key: 'f6741f68e7604ecd65621c682204b14b',
     text: query,
     format: 'json',
+    page: currentPage,
   }).done(processImages);
 };
 
-var processImages= function(result){
-  var photos = result.photos.photo;
+// CONTROL LOADING
+var requestInProgress = false;
+var currentPage = 1;
+var endOfResults = false;
 
-  _(photos).each(function(photo){
+var lateLoad = function (){
+  if (requestInProgress || endOfResults){
+    return;
+  }
+
+  var scrollTop = $(window).scrollTop();
+  var docHeight = $(document).height();
+  var windowHeight = $(window).height();
+
+  if (scrollTop === docHeight - windowHeight){
+    searchFlickr();
+  }
+};
+
+var clearPhotos = function () {
+  currentPage = 1;
+  endOfResults = false;
+  $('#images').empty();
+};
+
+var processImages= function(result){
+  requestInProgress = false;
+  currentPage += 1;
+
+  if (result.photos.photo.length === 0){
+    endOfResults = true;
+  }
+
+  if (result.stat != 'ok') {
+    return;
+  }
+
+// FLICKR URL CREATION
+  _(result.photos.photo).each(function(photo){
     var url = [
       'https://farm',
       photo.farm,
@@ -27,25 +81,7 @@ var processImages= function(result){
       '_q.jpg'
     ].join('');
 
-    var $img = $('<img>').attr('src', url);
+    var $img = $('<img>').attr('src', url).fadeIn('slow');
     $img.appendTo('#images');
   });
 };
-
-$(document).ready(function(){
-  $('#search').on('click', searchFlickr);
-  $('#query').on('keypress', function(event){
-    if(event.which !== 13){
-      return;
-    } 
-    searchFlickr();
-  });
-
-  var clearPhotos = function () {
-  currentPage = 1; // Reset the page number.
-  endOfResults = false;
-  $('#images').empty();
-};
-  $('#clear').on('click', clearPhotos);
-
-});
