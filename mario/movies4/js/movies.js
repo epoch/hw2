43337a543a483f495772d70f53ac01409950b478
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-24 15:44:23 +1100

    crude version

--- mario/movies4/js/movies.js
+++ mario/movies4/js/movies.js
@@ -1,10 +1,20 @@
+var insertPoster = function(movieJSON) {
+  $posterDiv = $('.poster');
+  title = movieJSON.Title;
+  year = movieJSON.Year;
+  $posterDiv.html('<h2>' + title + ', ' + year + '</h2>')
+
+  var $poster = $('<img>').attr('src', movieJSON.Poster);
+  $poster.appendTo($posterDiv);
+};
+
 var searchOMDB = function () {
   var movieTitle = $('#movie-title').val();
   var searchSlug = movieTitle.split(' ').join('+');
   var searchString = 'http://www.omdbapi.com/?t=' + searchSlug + '&plot=short&r=json';
 
   $.getJSON(searchString).done(function (result) {
-   console.log(result);
+   insertPoster(result);
   });
 };
 
