40d0d77416c4044f313d91742f28913bb64e5b49
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-24 15:27:42 +1100

    searches omdb

--- mario/movies4/js/movies.js
+++ mario/movies4/js/movies.js
@@ -1,11 +1,13 @@
-$(document).ready(function () {
-
-  var movieTitle = "true lies";
+var searchOMDB = function () {
+  var movieTitle = $('#movie-title').val();
   var searchSlug = movieTitle.split(' ').join('+');
   var searchString = 'http://www.omdbapi.com/?t=' + searchSlug + '&plot=short&r=json';
 
   $.getJSON(searchString).done(function (result) {
    console.log(result);
   });
+};
 
+$(document).ready(function () {
+  $('#search').on('click', searchOMDB);
 });
