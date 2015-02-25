6522e41d25d1c4c16faccf2b353a1334c6855abb
xandero <alexander.ohan@gmail.com>
2015-02-25 18:08:49 +1100

    Movies ajax added

--- xander/MovieAJAX/movieAJAX.js
+++ xander/MovieAJAX/movieAJAX.js
@@ -16,30 +16,29 @@ $(document).ready(function() {
         var $movieTitle = item.Title;
         var $movieYear = item.Year;
         var $movieID = item.imdbID;
-
-        $('#results-list').append('<li data-id="' + $movieID + '">' + $movieTitle + ', ' + $movieYear + '</li>').attr("class", "movie-list");
+        var $entry = $('<li><button>' + $movieTitle + ', ' + $movieYear + '</button></li>').attr("id", $movieID);
+        $('#results-list').append($entry);
 
         });
       }
     });
   });
 
-  $('.movie-list').on('click', 'li', function(event) {
-    event.preventDefault();
-    var $imdbID = $(this).attr('data-id');
-  // Need to make he list actual links so they can be clicked on and trigger this function.
+  $('#results-list').on('click', 'button', function() {
+    $('#show-poster').empty();
+    var $imdbID = $(this).parent().attr('id');
       $.ajax({
         url: 'http://www.omdbapi.com/?i=' + $imdbID,
         method: 'get',
         dataType: 'json',
         success: function(movie){
-             debugger;
-          // var $poster = item.Poster;
-         
-          }
-
-      
-        });     
-    });
+             
+          var $posterUrl = movie.Poster;
+          var $poster = $('<img>').attr('src', $posterUrl);
+          $('#show-poster').append($poster);
+          $('#results-list').empty();
+      }      
+    });     
+  });
 });
 
