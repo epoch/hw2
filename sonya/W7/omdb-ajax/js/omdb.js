41d58a6a2244c3e40d3a544cb06125c667899d0e
soxface <sonyacorcoran@gmail.com>
2015-02-26 09:08:53 +1100

    poster and search results displaying

--- sonya/W7/omdb-ajax/js/omdb.js
+++ sonya/W7/omdb-ajax/js/omdb.js
@@ -1,3 +1,4 @@
+// GET AND DISPLAY POSTER
 $(document).ready(function () {
 
   $('#lookup').on('click', function(event){
@@ -7,7 +8,7 @@ $(document).ready(function () {
 
     var processMovie = function (movie) {
       var $poster = $('<img>').attr('src', movie.Poster);
-      $('#result').html($poster);
+      $('#result-poster').html($poster);
     }
 
     var url = 'http://www.omdbapi.com/';
@@ -18,3 +19,44 @@ $(document).ready(function () {
 
 });
 
+// GET AND DISPLAY SEARCH RESULTS
+$(document).ready(function () {
+
+  $('#lookup').on('click', function(event){
+    event.preventDefault();
+
+    var errorWithRequest = function (message) {
+      console.log("There is an error!")
+    }
+
+    var showResults = function (movie) {
+      console.log(movie)
+      // debugger;
+      // var $movieList = $('<li/>').attr('li', movie.Search);
+      var $displayList = $('<ul>')
+      var $movieList = movie.Search
+      // $('#result-list').append( "<p>Or did you mean...</p>");
+      _($movieList).each(function(film){
+          var title = film.Title
+          var imdbID = film.imdbID
+          var $link = $('<li>' + title + '</li>').attr('id', imdbID)
+          $displayList.append($link)
+      });
+      $('#result-list').html($displayList);
+      
+      // LOOP TITLES & DISPLAY AS <li>
+
+
+      // showResults();
+    }
+
+    var query = $('#title').val();
+    var url = 'http://www.omdbapi.com/?';
+    $.getJSON(url, {
+      s: query,
+      type: 'movie',
+      r: 'json'
+    }).done(showResults).fail(errorWithRequest);
+  });
+
+});
